0085015042bdbc641975236cfe8b208421815627
LeKarimDerradji <karim.derradji.pro@gmail.com>
2021-06-23 13:43:12 +0200

    subtitle

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Heading } from '@chakra-ui/layout'
+import { Container, Heading, Text } from '@chakra-ui/layout'
 
 const Header = () => {
     return (
@@ -9,22 +9,24 @@ const Header = () => {
             height='50vh' 
             textAlign='center'
             d='flex'
+            flexDirection='column'
             justifyContent='center'
             alignItems='center'
-            sx={
             // credit : supremat.platz
-            {backgroundImage: "url('https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/cb204582849369.5d2a2f5c3c903.jpg')",
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundRepeat: 'no-repeat'
-            }}>
-
+            background='black'>
+        
             <Heading 
             as='h1' 
             color='red'
             fontWeight='light'
             fontSize='9xl'>BLOCK.MADNESS
             </Heading>
+            
+            <Text 
+            color='white'
+            fontSize='3xl'>
+                A Semi-Authoritarian Serious Game.  
+            </Text>
 
         </Container>
     )
